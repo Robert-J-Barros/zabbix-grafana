4c379be042508f65ddc2fe02b15258d7797df650
Robert-J-Barros <robert.junior.barros@gmail.com>
2024-06-19 13:11:50 -0300

    teste

--- zabbix.conf/keys/teste.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-# Comando SSH para se conectar ao servidor remoto e obter a contagem de pacotes atualizáveis
-result=$(ssh -i /home/brutforce/aws-zabbix/zabbix.conf/keys/airflow-prod.pem ubuntu@54.232.75.169 \
-    'sudo apt update > /dev/null && sudo apt list --upgradable | wc -l')
-
-# Imprimir o resultado localmente
-echo "$result"
-
